0b98df31ce875cf1ad77e828a5bbf35710272671
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-07 18:57:42 -0500

    completed skills and hobbies

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Express',
+    title: "Zo's Bio",
     pic: "https://avatars1.githubusercontent.com/u/16580180?v=3&amp;s=460",
-    bio: "I'm a party goer and like to jump off buildings.  Everything is nice and dandy"
+    bio: "I'm a party goer and like to jump off buildings.  Everything is nice and dandy",
+skillstitle: "My Skills",
+hobbiestitle: "My Hobbies",
+    skills: ['html','css','javascript','java'],
+    hobbies: ['boxing', 'reading', 'classical latin']
   });
 });
 
